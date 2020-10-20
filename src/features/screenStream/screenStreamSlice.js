4d72b841672d11aebe2c7301854ef9f8b0f4b769
Cameron Roe <cameron@cameronbroe.com>
2020-10-19 20:07:03 -0500

    Some format clean up

--- src/features/screenStream/screenStreamSlice.js
+++ src/features/screenStream/screenStreamSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {getWindowSize} from "../../api/wdaApi";
-import {selectSession} from "../wdaSession/wdaSessionSlice";
+import { getWindowSize } from "../../api/wdaApi";
+import { selectSession } from "../wdaSession/wdaSessionSlice";
 
 export const screenSessionSlice = createSlice({
   name: 'screenSession',
